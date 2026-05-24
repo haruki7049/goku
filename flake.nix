4cd83462335ebfdbdbe9c20d23aaae9a443255bb
haruki7049 <tontonkirikiri@gmail.com>
2026-05-24 19:42:03 +0900

    Update

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -22,8 +26,27 @@
       ];
 
       perSystem =
-        { pkgs, lib, ... }:
         {
+          config,
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          overlays = [ inputs.gomod2nix.overlays.default ];
+
+          goku = pkgs.buildGoApplication {
+            name = "goku";
+            src = lib.cleanSource ./.;
+            modules = ./gomod2nix.toml;
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+          };
+
           treefmt = {
             projectRootFile = ".git/config";
 
@@ -44,11 +67,23 @@
             programs.shfmt.enable = true;
           };
 
+          packages = {
+            inherit goku;
+            default = goku;
+          };
+
+          checks = {
+            inherit goku;
+          };
+
           devShells.default = pkgs.mkShell {
             nativeBuildInputs = [
               pkgs.go # Golang
               pkgs.nil # Nix LSP
+              pkgs.gomod2nix # gomod2nix for creating Hashes (./gomod2nix.toml)
             ];
+
+            inputsFrom = [ config.treefmt.build.devShell ];
           };
         };
     };
