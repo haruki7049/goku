0242a0339dc14ede6ccf8d88b2cd1fa2798be9b6
haruki7049 <tontonkirikiri@gmail.com>
2026-05-27 20:40:36 +0900

    Update

--- main.go
+++ main.go
@@ -5,16 +5,47 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Game struct{}
+type GameState int
+
+func (gs *GameState) Cycle() {
+	*gs += 1
+	if *gs > BarBar {
+		*gs = HelloWorld
+	}
+}
+
+const (
+	_ GameState = iota
+	HelloWorld
+	FooFoo
+	BarBar
+)
+
+type Game struct {
+	keys  []ebiten.Key
+	state GameState
+}
 
 func (g *Game) Update() error {
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		g.state.Cycle()
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	if g.state == HelloWorld {
+		ebitenutil.DebugPrint(screen, "Hello, World!")
+	} else if g.state == FooFoo {
+		ebitenutil.DebugPrint(screen, "FooFoo!")
+	} else if g.state == BarBar {
+		ebitenutil.DebugPrint(screen, "BarBar!")
+	} else {
+		ebitenutil.DebugPrint(screen, "Unknown GameState is found")
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -24,7 +55,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&Game{state: HelloWorld}); err != nil {
 		log.Fatal(err)
 	}
 }
